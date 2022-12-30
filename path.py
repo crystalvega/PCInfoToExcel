5ccaf0698ecefa049d1d149083630fe96b6788da
crystalvega <110161341+crystalvega@users.noreply.github.com>
2022-12-30 21:56:51 +0500

    Delete path.py

--- path.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-def resource_path(relative_path):
-    if getattr(sys, 'frozen', False):
-        base_path = sys._MEIPASS
-    else:
-        base_path = os.getcwd()
-    return os.path.join(base_path, relative_path)
