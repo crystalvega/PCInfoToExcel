3849632df4070f54f65de9f99a8659fd2da88984
crystalvega <110161341+crystalvega@users.noreply.github.com>
2022-12-30 21:54:50 +0500

    Delete getantivirus.py

--- GetConfiguration/getantivirus.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def get():
-    proc = subprocess.Popen('wmic /node:localhost /namespace:\\\\root\\SecurityCenter2 path AntiVirusProduct Get DisplayName | findstr /V /B /C:displayName || echo No Antivirus installed', shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, err = proc.communicate()
-    return out.decode().replace("\r", "").replace("\n", "")
